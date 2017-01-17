481eab4b53bfc85d7f7f766b8cede1672b815339
Jianye Xu <jianye.xu.stats@gmail.com>
2017-01-16 23:53:41 -0800

    make plot with candlesticks, moving averages and volume

---
+++ make_plot.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import datetime as dt
+from matplotlib.finance import candlestick_ohlc
+
+def plot_stock(data):
+    symbol = data["Symbol"][0]
+    data['Date'] = data.index
+    data['Date2'] = data['Date'].apply(lambda d: mdates.date2num(d.to_pydatetime()))
+    tuples = [tuple(x) for x in data[['Date2', 'Open', 'High', 'Low', 'Adj_Close']].values]
+    fig, ax = plt.subplots()
+    ax.grid(True)
+    ax.xaxis_date()
+    ax.xaxis.set_major_formatter(mdates.DateFormatter("%Y-%m-%d"))
+    plt.xticks(rotation=45)
+    plt.xlabel("Date")
+    plt.ylabel("Price")
+    plt.title(symbol)
+    candlestick_ohlc(ax, tuples, width=.6, colorup='g', alpha=.4)
+    ax.plot(data["Date2"], data["ema_17"])
+    ax.plot(data["Date2"], data["ema_43"])
+    ax2 = ax.twinx()
+    ax2.set_ylabel('Volume', color='r')
+    ax2.set_ylim(ymax=data["Volume"].max()*5)
+    ax2.bar(data["Date2"], data["Volume"])
+    plt.close()
+    return fig
+    #plt.show()
+
