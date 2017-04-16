071d8a1a570c283398451482ed03063219617711
Jianye Xu <jianye.xu.stats@gmail.com>
2017-04-15 22:28:21 -0700

    added to orgnize constants

--- lib/constants.py
+++ lib/constants.py
@@ -1,1 +1,5 @@
-SLICE_NONE = ["label": "no_slice", "db_column":"", "df_column":""]
+import pandas as pd
+
+report_today = pd.read_csv("C:\Users\\ralph\OneDrive\Documents\GitHub\stocks_pred\stocks\\report_today.csv",
+                           sep='\t')
+SYMBOLS = list(report_today.iloc[:,0].apply(lambda x: x.split("_")[0]))
